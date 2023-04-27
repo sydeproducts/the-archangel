341390fab45627df950eb8582e70296b652a311a
sydeco <products.syde@gmail.com>
2023-04-27 11:15:10 +0700

    add: attack counter

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+import { unsuccessful, successful, update, visits } from "../../config.js";
+
+//Disable right mouse click Script
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+// attack counter
+const unsucc = document.querySelector(".unsuccessful");
+const succ = document.querySelector(".successful");
+const up = document.querySelector(".update");
+const visit = document.querySelector(".visits");
+
+unsucc.innerText = unsuccessful;
+succ.innerText = successful;
+up.innerText = update;
+visit.innerText = visits;
+// end of attack counter
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
---
+++ config.js
@@ -0,0 +1,4 @@
+export var unsuccessful = 99999;
+export var successful = 0;
+export var visits = 0;
+export var update = "27 April 2023";
