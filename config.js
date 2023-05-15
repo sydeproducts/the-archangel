d05a5a8ef007736c4ce27eb85b2fa84d087c8379
sydeco <products.syde@gmail.com>
2023-05-15 08:54:00 +0700

    update daily

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export var unsuccessful = `2491`;
+export var unsuccessful = `4904`;
 export var successful = 0;
-export var visits = 22;
-export var update = "8 May 2023";
+export var visits = 24;
+export var update = "15 May 2023";
