e63fdf3ff9223cdf085d022d6b3b2f2d74c154e2
sydeco <products.syde@gmail.com>
2023-05-26 14:01:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const unsuccessful = `14391`;
 export const successful = 0;
-export const visits = 180;
-export const visits = 213;
+export const visits = 212;
 export const update = "26 May 2023";
